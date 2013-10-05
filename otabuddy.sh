47522e38cb877e92fdf21bd17f8a236e93b473d3
Glen Tregoning <glen.tregoning@asurion.com>
2013-10-04 17:41:49 -0700

    - Adding Small and Large Image Support - Adding Bundle Versioning Support - Adding App Name Helper function

--- otabuddy.sh
+++ otabuddy.sh
@@ -34,14 +34,37 @@ otaplist()
 		printusage
 		exit 1
 	else
+
+        BASE_URL=$(dirname $2)
 		# Extract IPA-files
 		APP_PLIST=temp.plist
 		OTA_PLIST=$3
-		unzip -p "$1" "**/Info.plist" > $APP_PLIST
+        # Handle occurance of multiple Info.plists in the ipa - choose the one with the shortest name (hopefully the root one)
+        INFO_PLIST_PATH=$(unzip -l "$1" "**/Info.plist" | awk '/-----/ {p = ++p % 2; next} p { s = ""; for (i = 4; i <= NF; i++) s = s $i " "; print s }' | awk '{ print length(), $0 | "sort -n" }' | cut -d " " -f 2- | head -n 1)
+
+        unzip -v -p "$1" "`echo $INFO_PLIST_PATH`" > $APP_PLIST
 
 		#Read contents
 		BUNDLE_IDENTIFIER=$($PLIST_BUDDY "Print CFBundleIdentifier" $APP_PLIST)
 		BUNDLE_NAME=$($PLIST_BUDDY "Print CFBundleDisplayName" $APP_PLIST)
+
+        ICON_NAME=$($PLIST_BUDDY "Print CFBundleIconFile" $APP_PLIST)
+        BUNDLE_VERSION=$($PLIST_BUDDY "Print CFBundleShortVersionString" $APP_PLIST)
+
+        # Extract Icon
+        echo Icon Name $ICON_NAME
+        ICON_PATH=$(unzip -l "$1" "**/$ICON_NAME" | awk '/-----/ {p = ++p % 2; next} p { s = ""; for (i = 4; i <= NF; i++) s = s $i " "; print s }')
+        echo "Icon Path" $ICON_PATH
+        unzip -j "$1" "`echo $ICON_PATH`"
+        mv $ICON_NAME image.57x57.png
+        ICON_NAME=image.57x57.png
+
+        # Extract Artwork
+        ARTWORK_PATH=$(unzip -l "$1" "**/iTunesArtwork" | awk '/-----/ {p = ++p % 2; next} p { s = ""; for (i = 4; i <= NF; i++) s = s $i " "; print s }')
+        echo "Artwork Path" $ARTWORK_PATH
+        unzip -j "$1" "`echo $ARTWORK_PATH`"
+        mv iTunesArtwork image.512x512.jpg
+        ARTWORK_NAME=image.512x512.jpg
 
 		# Clean up
 		rm $APP_PLIST
@@ -52,11 +75,22 @@ otaplist()
 		$PLIST_BUDDY "Add :items:0:metadata:bundle-identifier string $BUNDLE_IDENTIFIER" $OTA_PLIST
 		$PLIST_BUDDY "Add :items:0:metadata:title string $BUNDLE_NAME" $OTA_PLIST
 		$PLIST_BUDDY "Add :items:0:metadata:kind string software" $OTA_PLIST
+        $PLIST_BUDDY "Add :items:0:metadata:bundle-version string $BUNDLE_VERSION" $OTA_PLIST
+
 		$PLIST_BUDDY "Add :items:0:assets array" $OTA_PLIST
 		$PLIST_BUDDY "Add :items:0:assets:0 dict" $OTA_PLIST
 		$PLIST_BUDDY "Add :items:0:assets:0:kind string software-package" $OTA_PLIST
 		$PLIST_BUDDY "Add :items:0:assets:0:url string $2" $OTA_PLIST
-		
+
+        $PLIST_BUDDY "Add :items:0:assets:1 dict" $OTA_PLIST
+        $PLIST_BUDDY "Add :items:0:assets:1:kind string display-image" $OTA_PLIST
+        $PLIST_BUDDY "Add :items:0:assets:1:url string $BASE_URL/$ICON_NAME" $OTA_PLIST
+
+        $PLIST_BUDDY "Add :items:0:assets:1 dict" $OTA_PLIST
+        $PLIST_BUDDY "Add :items:0:assets:1:kind string full-size-image" $OTA_PLIST
+        $PLIST_BUDDY "Add :items:0:assets:1:url string $BASE_URL/$ARTWORK_NAME" $OTA_PLIST
+
+
 		echo "Created $OTA_PLIST with values:"
 		echo "Bundle identifier: $BUNDLE_IDENTIFIER"
 		echo "Title:             $BUNDLE_NAME"
@@ -76,6 +110,27 @@ itms()
 		echo "Example HTML anchor:"
 		echo "<a href=\"itms-services://?action=download-manifest&url=$(urlencode $1)\">Download my application</a>"
 	fi
+}
+
+appname() {
+    if [ -z "$1" ]; then
+        echo "Missing IPA parameter"
+        printusage
+        exit 1
+    else
+        # Extract IPA-files
+        APP_PLIST=temp.plist
+        OTA_PLIST=$3
+        # unzip -p "$1" "**/Info.plist" > $APP_PLIST
+        # Handle occurance of multiple Info.plists in the ipa - choose the one with the shortest name (hopefully the root one)
+        INFO_PLIST_PATH=$(unzip -l "$1" "**/Info.plist" | awk '/-----/ {p = ++p % 2; next} p { s = ""; for (i = 4; i <= NF; i++) s = s $i " "; print s }' | awk '{ print length(), $0 | "sort -n" }' | cut -d " " -f 2- | head -n 1)
+
+        unzip -v -p "$1" "`echo $INFO_PLIST_PATH`" > $APP_PLIST
+
+        #Read contents
+        BUNDLE_NAME=$($PLIST_BUDDY "Print CFBundleDisplayName" $APP_PLIST)
+        echo $BUNDLE_NAME
+    fi
 }
 
 printusage()
@@ -107,6 +162,9 @@ case "$1" in
 		itms)
 			itms "$2"
 			;;
+        appname)
+            appname "$2"
+        ;;
 		*)
 			printusage
 			
